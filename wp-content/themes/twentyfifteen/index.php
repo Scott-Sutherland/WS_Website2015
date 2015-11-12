0f656efc61163dc35bcc4a0ad703adc80f074ee7
Scott Sutherland <Scott@we-shape.co.uk>
2015-11-12 23:08:10 +0000

    Test Push

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -19,6 +19,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		Test
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
